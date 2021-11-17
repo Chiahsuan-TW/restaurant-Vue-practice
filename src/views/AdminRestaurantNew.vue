45da0451c58621a2c357869d69bb011b2f5fcd37
Chiahsuan Chou <belinda05021992@gmail.com>
2021-11-17 15:13:27 +0800

    new form with v-model

---
+++ src/views/AdminRestaurantNew.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="container py-5">
+    <!-- 餐廳表單 AdminRestaurantForm -->
+    <h1>餐廳表單 </h1>
+    <AdminRestaurantForm/>
+  </div>
+</template>
+
+
+<script>
+import AdminRestaurantForm from "./../components/AdminRestaurantForm"
+
+export default {
+  components: {
+    AdminRestaurantForm,
+  }
+}
+</script>
